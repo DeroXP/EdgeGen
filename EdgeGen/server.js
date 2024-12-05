85a8c41ca8edde96bac5facc7a0819aadde8bd1c
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-12-05 13:16:59 -0600

    Update server.js

--- EdgeGen/server.js
+++ EdgeGen/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require("body-parser");
-const fetch = require('node-fetch');
 const { spawn } = require("child_process");
 
 const app = express();
